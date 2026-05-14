7a5ea443414cd9017ebaacd8252126ca4d040d09
yasin-arafat-05 <yasinarafat.e2021@gmail.com>
2026-05-14 21:12:17 +0600

    first-class-assignment

--- lib/1_assignment.dart
+++ lib/1_assignment.dart
@@ -1,9 +1,23 @@
-import "dart:io"
-int main(){
-  stdout.write("Enter the income list");
-  String? income = stdin.readLineSync();
-  stdout.write("Enter the expense list");
-  String? expense = stdin.readLineSync();
-  
+// ignore_for_file: avoid_print
+
+import "dart:io";
+import "dart:math";
+
+int main() {
+  stdout.write("Enter the income list: \n");
+  String income = stdin.readLineSync() ?? " ";
+  stdout.write("Enter the expense list: \n");
+  String expense = stdin.readLineSync() ?? " ";
+
+  List<int> incomes = income.split(" ").map(int.parse).toList();
+  List<int> expenses = expense.split(" ").map(int.parse).toList();
+
+  int length = max(incomes.length, expenses.length);
+
+  for (int i = 0; i <= length; i++) {
+    int inc = incomes[i];
+    int exp = expenses[i];
+    print("income: $inc  expense: $exp difference: ${inc - exp}");
+  }
   return 0;
 }
