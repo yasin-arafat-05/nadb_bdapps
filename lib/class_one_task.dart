73bd9891b5ba4a886957eefdf926ec0750d561f9
yasin-arafat-05 <yasinarafat.e2021@gmail.com>
2026-05-14 23:37:37 +0600

    class_one_assignment

--- lib/class_one_task.dart
+++ lib/class_one_task.dart
@@ -1,41 +1,128 @@
-// ignore_for_file: avoid_print
+import "dart:io";
 
 int main() {
-  final List<Map<dynamic, dynamic>> inexp = [
-    {'income': 2000.2, 'expense': 100.0},
-    {'income': 3000.4, 'expense': 200.2},
-    {'income': 4000.12, 'expense': 400.1},
-  ];
+  //
+  // dynmic can automatically handle null safety.
+  //
+  final List<Map<dynamic, dynamic>> data = [];
 
-  for (var x in inexp) {
-    double income = x['income'] as double;
-    double expense = x['expense'] as double;
-    print('difference : ${income - expense}');
+  // ============ take input from cli: ===============
+  stdout.write("Enter the total number of customers <1 to 9>: ");
+  int num = int.parse(stdin.readLineSync()!);
+  while (num != 0) {
+    takeInput(data);
+    num = num - 1;
   }
 
-  // task assending and descending order vlaue:
-  final List<Map<String, dynamic>> student = [
-    {'name': 'yasin', 'score': 80},
-    {'name': 'arafat', 'score': 20},
-    {'name': 'nilla', 'score': 60},
-    {'name': 'zorina', 'score': 30},
-  ];
+  int cnt = 1;
 
-  // desding  order
-  student.sort((a, b) => (b['score'] as int).compareTo(a['score'] as int));
-  print(student);
+  for (var val in data) {
+    String? name = val["Name"];
+    double income = val["Income"] ?? -1;
+    double food = val["Food"] ?? -1;
+    double rent = val["Rent"] ?? -1;
+    double trans = val['Trans'] ?? -1;
 
-  // assending  order
-  student.sort((a, b) => (a['score'] as int).compareTo(b['score'] as int));
-  print(student);
+    if (food == -1) {
+      print("Food Value is not entered correctly");
+    }
+    if (rent == -1) {
+      print("Food Value is not entered correctly");
+    }
+    if (trans == -1) {
+      print("Food Value is not entered correctly");
+    }
 
-  // filter:
-  print('============Iterable ouput=======================');
-  final low = student.where((arr) => arr['score'] < 80);
-  print(low);
+    print("");
+    print("============ Person No: $cnt =================");
+    print("Person Name: $name");
+    print("Income: $income");
+    print("Food Expense: $food");
+    print("Rent Expense: $rent");
+    print("Transport Expense: $trans");
+    print("----------------------------");
+    print("Total Expense: ${food + rent + trans}");
 
-  print('============Iterable to list=======================');
-  final lower = student.where((arr) => arr['score'] < 80).toList();
-  print('$lower');
+    double dif = income - (food + rent + trans);
+    if (dif < 0) {
+      print("Tomar income theke expense beci.");
+    } else {
+      print("Saving: $dif");
+    }
+
+    cnt += 1;
+  }
   return 0;
 }
+
+void takeInput(List<Map<dynamic, dynamic>> data) {
+  // take input customer name
+  stdout.write("Enter Name: ");
+  String? name = stdin.readLineSync();
+
+  // take input income expense
+  stdout.write("Enter Income: ");
+  String? incm = stdin.readLineSync();
+  double? income = double.tryParse(incm ?? "");
+
+  // take input food expense
+  stdout.write("Enter Food Expense: ");
+  String? fe = stdin.readLineSync();
+  double? foodExpense = double.tryParse(fe ?? "");
+
+  // take input rent expense
+  stdout.write("Enter rent Expense: ");
+  String? re = stdin.readLineSync();
+  double? rentExpense = double.tryParse(re ?? "");
+
+  // take input transport expense
+  stdout.write("Enter rent Expense: ");
+  String? te = stdin.readLineSync();
+  double? transExpense = double.tryParse(te ?? "");
+
+  data.add({
+    "Name": name,
+    "Income": income,
+    "Food": foodExpense,
+    "Rent": rentExpense,
+    "Trans": transExpense,
+  });
+}
+
+/*
+================================ output of the code: ===========================
+===============================copiedfrom termnal D:)===========================
+
+Enter the total number of customers <1 to 9>: 2
+Enter Name: Yasin
+Enter Income: 12
+Enter Food Expense: 12.0
+Enter rent Expense: 12.0
+Enter rent Expense: 12.0
+Enter Name: Arafat
+Enter Income: 500
+Enter Food Expense: 12.0
+Enter rent Expense: 5.0
+Enter rent Expense: 5.0
+
+============ Person No: 1 =================
+Person Name: Yasin
+Income: 12.0
+Food Expense: 12.0
+Rent Expense: 12.0
+Transport Expense: 12.0
+----------------------------
+Total Expense: 36.0
+Tomar income theke expense beci.
+
+============ Person No: 2 =================
+Person Name: Arafat
+Income: 500.0
+Food Expense: 12.0
+Rent Expense: 5.0
+Transport Expense: 5.0
+----------------------------
+Total Expense: 22.0
+Saving: 478.0
+
+*/
