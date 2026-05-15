209edabfb43f38b17ac06afdc34c2c6e7c4de508
yasin-arafat-05 <yasinarafat.e2021@gmail.com>
2026-05-15 09:16:20 +0600

    class_one_assignment

--- lib/1_assignment.dart
+++ lib/1_assignment.dart
@@ -1,48 +1,128 @@
-// ignore_for_file: avoid_print
-
 import "dart:io";
 
-void main() {
-  // Income input
-  stdout.write("Enter income: ");
-  int income = int.tryParse(stdin.readLineSync() ?? "") ?? 0;
+int main() {
+  //
+  // dynmic can automatically handle null safety.
+  //
+  final List<Map<dynamic, dynamic>> data = [];
 
-  // Expense inputs
-  stdout.write("Enter food expense: ");
-  int food = int.tryParse(stdin.readLineSync() ?? "") ?? 0;
+  // ============ take input from cli: ===============
+  stdout.write("Enter the total number of customers <1 to 9>: ");
+  int num = int.parse(stdin.readLineSync()!);
+  while (num != 0) {
+    takeInput(data);
+    num = num - 1;
+  }
 
-  stdout.write("Enter rent expense: ");
-  int rent = int.tryParse(stdin.readLineSync() ?? "") ?? 0;
+  int cnt = 1;
 
-  stdout.write("Enter transport expense: ");
-  int transport = int.tryParse(stdin.readLineSync() ?? "") ?? 0;
+  for (var val in data) {
+    String? name = val["Name"];
+    double income = val["Income"] ?? -1;
+    double food = val["Food"] ?? -1;
+    double rent = val["Rent"] ?? -1;
+    double trans = val['Trans'] ?? -1;
 
-  // Total expense
-  int totalExpense = food + rent + transport;
+    if (food == -1) {
+      print("Food Value is not entered correctly");
+    }
+    if (rent == -1) {
+      print("Food Value is not entered correctly");
+    }
+    if (trans == -1) {
+      print("Food Value is not entered correctly");
+    }
 
-  // Remaining balance
-  int balance = income - totalExpense;
+    print("");
+    print("============ Person No: $cnt =================");
+    print("Person Name: $name");
+    print("Income: $income");
+    print("Food Expense: $food");
+    print("Rent Expense: $rent");
+    print("Transport Expense: $trans");
+    print("----------------------------");
+    print("Total Expense: ${food + rent + trans}");
 
-  // Savings percentage
-  double savings = (balance / income) * 100;
+    double dif = income - (food + rent + trans);
+    if (dif < 0) {
+      print("Tomar income theke expense beci.");
+    } else {
+      print("Saving: $dif");
+    }
 
-  // Map for categories (Bonus)
-  Map<String, int> expenses = {
-    "Food": food,
-    "Rent": rent,
-    "Transport": transport,
-  };
-
-  // Output
-  print("\n===== Finance Summary =====");
-  print("Income: $income");
-
-  print("\nExpenses:");
-  expenses.forEach((key, value) {
-    print("$key: $value");
-  });
-
-  print("\nTotal Expense: $totalExpense");
-  print("Remaining Balance: $balance");
-  print("Savings Percentage: ${savings.toStringAsFixed(2)}%");
+    cnt += 1;
+  }
+  return 0;
 }
+
+void takeInput(List<Map<dynamic, dynamic>> data) {
+  // take input customer name
+  stdout.write("Enter Name: ");
+  String? name = stdin.readLineSync();
+
+  // take input income expense
+  stdout.write("Enter Income: ");
+  String? incm = stdin.readLineSync();
+  double? income = double.tryParse(incm ?? "");
+
+  // take input food expense
+  stdout.write("Enter Food Expense: ");
+  String? fe = stdin.readLineSync();
+  double? foodExpense = double.tryParse(fe ?? "");
+
+  // take input rent expense
+  stdout.write("Enter rent Expense: ");
+  String? re = stdin.readLineSync();
+  double? rentExpense = double.tryParse(re ?? "");
+
+  // take input transport expense
+  stdout.write("Enter rent Expense: ");
+  String? te = stdin.readLineSync();
+  double? transExpense = double.tryParse(te ?? "");
+
+  data.add({
+    "Name": name,
+    "Income": income,
+    "Food": foodExpense,
+    "Rent": rentExpense,
+    "Trans": transExpense,
+  });
+}
+
+/*
+================================ output of the code: ===========================
+===============================copiedfrom termnal D:)===========================
+
+Enter the total number of customers <1 to 9>: 2
+Enter Name: Yasin
+Enter Income: 12
+Enter Food Expense: 12.0
+Enter rent Expense: 12.0
+Enter rent Expense: 12.0
+Enter Name: Arafat
+Enter Income: 500
+Enter Food Expense: 12.0
+Enter rent Expense: 5.0
+Enter rent Expense: 5.0
+
+============ Person No: 1 =================
+Person Name: Yasin
+Income: 12.0
+Food Expense: 12.0
+Rent Expense: 12.0
+Transport Expense: 12.0
+----------------------------
+Total Expense: 36.0
+Tomar income theke expense beci.
+
+============ Person No: 2 =================
+Person Name: Arafat
+Income: 500.0
+Food Expense: 12.0
+Rent Expense: 5.0
+Transport Expense: 5.0
+----------------------------
+Total Expense: 22.0
+Saving: 478.0
+
+*/
